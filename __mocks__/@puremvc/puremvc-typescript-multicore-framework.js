dc1958db1196a5c47ae9834d9b97930b584c0321
cliffhall <cliff@futurescale.com>
2025-02-26 16:40:44 -0500

    Unit tests working via requireActual passthrough mocks.

--- __mocks__/@puremvc/puremvc-typescript-multicore-framework.js
+++ __mocks__/@puremvc/puremvc-typescript-multicore-framework.js
@@ -1,5 +1,5 @@
 'use strict';
 const PUREMVC = '../../node_modules/@puremvc/puremvc-typescript-multicore-framework';
 
-// THIS IS THE MOST ABSURD THING I"VE EVER HAD TO DO TO MAKE A TEST SUITE WORK.
+// THIS IS THE MOST ABSURD THING I'VE EVER HAD TO DO TO MAKE A TEST SUITE WORK.
 module.exports = jest.requireActual(PUREMVC);
